3de3ba6bee206e937064dd6d62ec7ac58cd81423
Damon Sorensen <damonsorensen@gmail.com>
2019-04-03 08:44:30 -0600

    initail commit

--- src/index.js
+++ src/index.js
@@ -1,36 +1,46 @@
 import Senators from './data/senators'
 
 export const republicans = () => {
-  return REPLACE_ME_WITH_CODE
+  return Senators.filter(senator=> senator.party === 'Republican' )
 }
 
 export const democrats = () => {
-  return REPLACE_ME_WITH_CODE
+  return Senators.filter(senator=> senator.party === 'Democrat')
 }
 
 export const independents = () => {
-  return REPLACE_ME_WITH_CODE
+  return Senators.filter(senator=> senator.party === 'Independent')
 }
 
 export const males = () => {
-  return REPLACE_ME_WITH_CODE
+  return Senators.filter(senator=> senator.person.gender_label === 'Male')
 }
 
 export const females = () => {
-  return REPLACE_ME_WITH_CODE
+  return Senators.filter(senator=> senator.person.gender_label === 'Female')
 }
 
 export const byState = (state = 'UT') => {
-  return REPLACE_ME_WITH_CODE
+  return Senators.filter(senator=> senator.state === 'UT')
 }
 
 export const mapping = () => {
-  return REPLACE_ME_WITH_CODE
+  return Senators.map(senator=> ({
+   firstName: senator.person.firstname,
+   lastName: senator.person.lastname,
+   party: senator.party,
+   gender: senator.person.gender_label
+  })
+  )
 }
-
 export const birthplaceSeniorSenator = () => {
-  return REPLACE_ME_WITH_CODE
+  return Senators.filter((senator=> senator.state === 'AZ'){
+    
+  })
+
+
+
 }
 
 
-const REPLACE_ME_WITH_CODE = false
+// const REPLACE_ME_WITH_CODE = false
